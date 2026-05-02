e5af35a13715f658fd038e5f8a4ab59b1c26423c
Rahul Kumar <rahulsinghrps9211@gmail.com>
2026-05-02 13:55:16 +0530

    Initial Commit

---
+++ GalleryApp/GalleryAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  GalleryAppApp.swift
+//  GalleryApp
+//
+//  Created by Rahul Kiumar on 02/05/26.
+//
+
+import SwiftUI
+
+@main
+struct GalleryAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
